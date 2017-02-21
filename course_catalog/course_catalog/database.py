582be27f01338c7041897eb006595ebda7e5661b
byanofsky <byanofsky@me.com>
2017-02-20 22:36:01 -0800

    Fix style issue in code

--- course_catalog/course_catalog/database.py
+++ course_catalog/course_catalog/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-
 engine = create_engine('sqlite:///../instance/course_catalog.db',
                        convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
