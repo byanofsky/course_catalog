c6e63f8a054b176cf7fdeee86f82e09178568a3d
byanofsky <byanofsky@me.com>
2017-02-22 10:57:49 -0800

    Settings changes for database

--- course_catalog/course_catalog/course_catalog.py
+++ course_catalog/course_catalog/course_catalog.py
@@ -1,9 +1,10 @@
-import sqlite3
+# import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 from database import db_session
 
-app = Flask(__name__, instance_relative_config=True)
+# app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__)
 app.config.from_object('course_catalog.default_settings')
 app.config.from_pyfile('config.py', silent=True)
 # app.run(host='0.0.0.0', port=5000)
--- course_catalog/course_catalog/default_settings.py
+++ course_catalog/course_catalog/default_settings.py
@@ -1,6 +1,2 @@
-import os
-
-
-DATABASE = 'course_catalog.db'
 SECRET_KEY = 'development key'
 DEBUG = True
