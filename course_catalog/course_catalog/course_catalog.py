fc1a2a6f48446f5eeaf7a60385b9fe5c6628f9f4
byanofsky <byanofsky@me.com>
2017-02-22 10:59:24 -0800

    Todo for debug settings

--- course_catalog/course_catalog/course_catalog.py
+++ course_catalog/course_catalog/course_catalog.py
@@ -6,6 +6,7 @@ from database import db_session
 # app = Flask(__name__, instance_relative_config=True)
 app = Flask(__name__)
 app.config.from_object('course_catalog.default_settings')
+# TODO: Why debug not working?
 app.config.from_pyfile('config.py', silent=True)
 # app.run(host='0.0.0.0', port=5000)
 
