7c9dd6369f63e989b189eebcd5cf7be4148ccbb1
byanofsky <byanofsky@me.com>
2017-03-06 12:13:05 -0800

    Change file location of default settings

--- course_catalog/course_catalog/course_catalog.py
+++ course_catalog/course_catalog/course_catalog.py
@@ -4,11 +4,10 @@ from database import db_session
 
 # app = Flask(__name__, instance_relative_config=True)
 app = Flask(__name__)
-app.config.from_object('course_catalog.default_settings')
+app.config.from_pyfile('default_settings.py')
 # TODO: Why debug not working?
 app.config.from_pyfile('config.py', silent=True)
 # app.run(host='0.0.0.0', port=5000)
-
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
