b139e86b206a8e2209336284cdb246f4d5eea674
byanofsky <byanofsky@me.com>
2017-03-06 15:30:42 -0800

    Adds unit testing with empty db test and registration test

---
+++ course_catalog/tests/test_course_catalog.py
@@ -0,0 +1,43 @@
+import os
+import course_catalog
+import unittest
+import tempfile
+
+class CourseCatalogTestCase(unittest.TestCase):
+    def setUp(self):
+        self.test_db_path = 'tests/test.db'
+        course_catalog.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../' + self.test_db_path
+        course_catalog.app.config['TESTING'] = True
+        self.app = course_catalog.app.test_client()
+        with course_catalog.app.app_context():
+            course_catalog.course_catalog.init_db()
+
+    def tearDown(self):
+        os.unlink(os.path.abspath(self.test_db_path))
+
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert b'No courses' in rv.data
+
+    def register(self, email, name, password, verify_password):
+        return self.app.post('/register/', data=dict(
+            email=email,
+            name=name,
+            password=password,
+            verify_password=verify_password,
+        ), follow_redirects=True)
+
+    def test_registration(self):
+        rv = self.register('by@me.com', 'Brandon', '12345', '12345')
+        assert b'You were successfully registered' in rv.data
+        rv = self.register('by', 'Brandon', '12345', '12345')
+        assert b'Please enter a valid email address.' in rv.data
+        rv = self.register('by@me.com', 'Brandon', '12345', '12345')
+        assert b'A user already exists with this email address.' in rv.data
+        rv = self.register('by@me.com', 'Brandon', '12', '12')
+        assert b'Please enter a valid password, at least 3 characters long.' in rv.data
+        rv = self.register('by@me.com', 'Brandon', '12345', '56789')
+        assert b'Please re-enter your password correctly.' in rv.data
+
+if __name__ == '__main__':
+    unittest.main()
