4c729de10952dd7fd9d2a789dbe66d14cd79a7dd
byanofsky <byanofsky@me.com>
2017-03-25 11:49:39 -0700

    Finetune and add comments to course_catalog.py

--- course_catalog/course_catalog/course_catalog.py
+++ course_catalog/course_catalog/course_catalog.py
@@ -1,16 +1,19 @@
-# import sqlite3
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+# Set filenames relative to 'instance' directory
 app = Flask(__name__, instance_relative_config=True)
-# app = Flask(__name__)
+# Attempt to use instance/config.py. If does not exist, uses default.config
 app.config.from_object('course_catalog.default_config')
-# TODO: Why debug not working?
 app.config.from_pyfile('config.py', silent=True)
-# app.run(host='0.0.0.0', port=5000)
+# Create database instance
 db = SQLAlchemy(app)
 
+# Import statement not at top as recommended in Flask docs
 import views
 
+
+# Function that can be used to initiate database
+# TODO: might be able to add this to a setup file
 def init_db():
     db.create_all()
